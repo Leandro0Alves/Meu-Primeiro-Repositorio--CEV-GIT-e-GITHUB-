d6755a673903c3843e4a430bc478cb4a76e80952
Leandro Alves <la.silva5290@gmail.com>
2024-01-12 18:32:02 -0300

    Background e cores dos txts mudam também
    adicionei textos e background que se altera de acordo com a hora.

--- Curso em Video/JavaScript/_exercicios/_site_horario_atual/_script.js
+++ Curso em Video/JavaScript/_exercicios/_site_horario_atual/_script.js
@@ -1,24 +1,43 @@
 function carregar() {
 
     var msg = window.document.getElementById('msg')
-
     var img = window.document.getElementById('imagem')
-
     var data = new Date()
-
     var hora = data.getHours()
+    var titulo = document.getElementById('titulo')
+    var arroba = document.getElementById('arroba')
+
 
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 5 && hora < 12) {
         img.src = '_images/001.jpg'
+        document.body.style.background = '#E6B369'
+        titulo.style.color = '#593202'
+        arroba.style.color = '#593202'
+
     } else if (hora >= 12 && hora < 16) {
         img.src = '_images/002.jpg'
+        document.body.style.background = '#88C9F2'
+        titulo.style.color = '#2A2C30'
+        arroba.style.color = '#2A2C30'
+        
     } else if (hora >= 16 && hora < 18){
         img.src = '_images/003.jpg'
+        document.body.style.background = '#F27405'
+        titulo.style.color = '#260801'
+        arroba.style.color = '#260801'
+
     } else if (hora >= 18 && hora < 24) {
         img.src = '_images/004.jpg'
+        document.body.style.background = '#1F2C3F'
+        titulo.style.color = 'white'
+        arroba.style.color = 'white'
+
     } else {
         img.src = '_images/005.jpg'
+        document.body.style.background = '#151110'
+        titulo.style.color = '#FEFFEA'
+        arroba.style.color = '#FEFFEA'
     }
 }
